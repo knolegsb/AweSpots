769b53d708316a8fe0815eb3c9f7374541dba332
Sean <seon2you@hotmail.com>
2017-02-22 12:15:22 -0800

    adding angular

---
+++ AweSpot/Scripts/app/AweSpotApp.js
@@ -0,0 +1,21 @@
+﻿var AweSpotApp = angular.module("AweSpotApp", ["ngRoute", "ui.bootstrap", "ngCookies", "ngSanitize"]);
+
+var config = function ($routeProvider, $locationProvider, $sceProvider) {
+    $routeProvider
+    .when("/", {
+        templateUrl: "/Views/Home/AweSpot.html",
+        controller: "AweSpotController"
+    })
+    .when("/login", {
+        tempaleUrl: "/Views/Account/Login.html",
+        controller: "LoginController"
+    })
+    .when("/register", {
+        templateUrl: "/Views/Account/Register.html",
+        controller: "RegisterController"
+    })
+    .when("/users", {
+        templateUrl: "/Views/Admin/Users.html",
+        controller: "UsersController"
+    });
+};
